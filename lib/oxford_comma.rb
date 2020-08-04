f54f72537a71a088a5ef200150242c7bf9c4a25b
Alexandra Gieger <giegeralexandra@gmail.com>
2020-08-04 01:34:52 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,11 @@
 def oxford_comma(array)
-  length = array.length
-  
-  return new_value.pop
   last_index = array[-1]
   last_value = array[last_index]
   array.pop
-  new_string = (array.join(", ")) + ", and ${last_value}"
+  if array[-1] <1
+    puts array
+    else 
+     new_string = (array.join(", ")) + ", and ${last_value}"
+  end
   return new_string
 end
