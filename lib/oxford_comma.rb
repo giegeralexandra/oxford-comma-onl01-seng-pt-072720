d783037e47678604a1201b3699f42fd8157719e5
Alexandra Gieger <giegeralexandra@gmail.com>
2020-08-04 01:42:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,10 @@
 def oxford_comma(array)
-  if array[-1] <1 
+  if array[-1] < 1 
     return array.join
   else 
     last_value = array.last
     array.pop
     array << "and #{last_value}"
     return array.join ", "
+  end
 end
