0ed004a05eb371e1ba15844880ba52985c7076aa
Alexandra Gieger <giegeralexandra@gmail.com>
2020-08-04 01:52:34 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,15 @@
 def oxford_comma(array)
   if array.length < 1 
     return array.join
-  else 
-    last_value = array[-1]
-    array.pop
-    second_last = array[-1]
-    array.pop
-    combined = "#{second_last} and #{last_value}"
-    array << "#{combined}"
-    return array.join ", "
-  end
+    elsif array.length = 1 
+      return array.join "and"
+    else 
+      last_value = array[-1]
+      array.pop
+      second_last = array[-1]
+      array.pop
+      combined = "#{second_last} and #{last_value}"
+      array << "#{combined}"
+      return array.join ", "
+    end
 end
