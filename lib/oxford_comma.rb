64cb6d5f24e60e774583d316adba5aacf4525d34
Alexandra Gieger <giegeralexandra@gmail.com>
2020-08-04 01:58:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length < 1 
     return array.join
   elsif array.length < 2
-      return array.join "and"
+      return array.join
     else 
       last_value = array[-1]
       array.pop
